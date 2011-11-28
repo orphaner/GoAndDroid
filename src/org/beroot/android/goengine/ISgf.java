2d49b1e1d94418988035c393eea7f02696804fed
Nicolas L. <nicolas@beroot.org>
2011-11-28 22:26:20 +0100

    Ajout de l'interface ISgf qui liste les propriétés

---
+++ src/org/beroot/android/goengine/ISgf.java
@@ -0,0 +1,59 @@
+package org.beroot.android.goengine;
+
+public interface ISgf
+{
+  /**
+   * Taille du goban
+   */
+  public static final String SZ = "SZ";
+
+  /**
+   * Nom du joueur blanc
+   */
+  public static final String PW = "PW";
+
+  /**
+   * Nom du joueur noir
+   */
+  public static final String PB = "PB";
+
+  /**
+   * Coup blanc
+   */
+  public static final String W = "W";
+
+  /**
+   * Coup noir
+   */
+  public static final String B = "B";
+
+  /**
+   * Liste de coups noirs
+   */
+  public static final String AB = "AB";
+
+  /**
+   * Liste de coups blancs
+   */
+  public static final String AW = "AW";
+
+  /**
+   * Liste de coups vides
+   */
+  public static final String AE = "AE";
+
+  /**
+   * Nombre de pierres de handicap
+   */
+  public static final String HA = "HA";
+
+  /**
+   * Couleur du joueur qui doit jouer en premier
+   */
+  public static final String PL = "PL";
+
+  /**
+   * Commentaire
+   */
+  public static final String C = "C";
+}
